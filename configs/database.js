f6e0f01c927e3872cb3dfa3fcb693c2b045f8e1b
Harshil Mendpara <75371580+HarshilMendpara@users.noreply.github.com>
2021-07-21 23:31:54 +0530

    Update database.js

--- configs/database.js
+++ configs/database.js
@@ -1,13 +1,11 @@
 const { Client } = require('pg');
 
-// const client = new Client({
-//     user: process.env.PSQL_USER,
-//     host: process.env.PSQL_HOST,
-//     database: process.env.PSQL_DATABASE,
-//     password: toString(process.env.PSQL_PASSWORD),
-//     port: 5432,
-// });
-
-const client = new Client('postgres://jelmxbot:cEYNZoBcAc0h8EKPO6ROUO-HqPVwJEsl@john.db.elephantsql.com/jelmxbot');
+const client = new Client({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: toString(process.env.PSQL_PASSWORD),
+    port: 5432,
+});
 
 module.exports = client;
